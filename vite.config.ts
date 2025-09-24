6187fcdf1f3e4d7e43993337f4c6c213904de0e6
akizono <aiyedoki@gmail.com>
2025-09-24 15:36:25 +0800

    chore: 更新環境變數配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { resolve } from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 
 import { createVitePlugins } from './build/plugins'
-import { createViteProxy } from './build/proxy'
-import { serviceConfig } from './src/utils/request/proxy/service.config'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // 根據當前工作目錄中的 `mode` 載入 .env 文件
   const env = loadEnv(mode, __dirname, '') as ImportMetaEnv
-  const envConfig = serviceConfig[mode as ServiceEnvType]
   return {
     base: env.VITE_BASE_PATH,
     plugins: createVitePlugins(env),
@@ -17,11 +14,6 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': resolve(__dirname, 'src'),
       },
-    },
-    server: {
-      host: '0.0.0.0',
-      proxy:
-        env.VITE_HTTP_PROXY === 'Y' ? createViteProxy(envConfig) : undefined,
     },
     build: {
       target: 'esnext',
